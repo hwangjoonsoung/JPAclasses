070c8812fef8f2a392ff0db788263087faba59ea
hwangjoonsoung <joonsoung93@naver.com>
2025-12-16 22:54:33 +0900

    query dsl : day 1

---
+++ query_dsl/src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,50 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.aspectj.weaver.patterns.HasThisTypePatternTriedToSneakInSomeGenericOrParameterizedTypePatternMatchingStuffAnywhereVisitor;
+
+import java.io.PipedReader;
+
+@Entity
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    // 연관관계 편의메서드
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+}
---
+++ query_dsl/src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,49 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonWriter;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+public class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("test entity")
+    void testEntity() throws Exception {
+        //given
+        Team teamA = new Team("team A");
+        Team teamB = new Team("team B");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        //when
+        em.flush();
+        em.clear();
+        //then
+        List<Member> resultList = em.createQuery("select m from Member  m", Member.class).getResultList();
+
+        for (Member member : resultList) {
+            System.out.println(member);
+        }
+
+    }
+}
